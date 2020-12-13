bbf8c22740de345debf064920cbc5f5e04ecb947
szkmiyabi <led.centipede@gmail.com>
2020-12-14 01:33:46 +0900

    DataGridFormを追加

---
+++ EffectiveEx/DataGridForm.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace EffectiveEx
+{
+    partial class DataGridForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.gridTableView = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gridTableView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.gridTableView, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 87.21461F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 12.78539F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(868, 490);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // gridTableView
+            // 
+            this.gridTableView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridTableView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridTableView.Location = new System.Drawing.Point(3, 3);
+            this.gridTableView.Name = "gridTableView";
+            this.gridTableView.RowHeadersWidth = 49;
+            this.gridTableView.RowTemplate.Height = 23;
+            this.gridTableView.Size = new System.Drawing.Size(862, 421);
+            this.gridTableView.TabIndex = 0;
+            // 
+            // DataGridForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 490);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "DataGridForm";
+            this.Text = "DataGridForm";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.gridTableView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView gridTableView;
+    }
+}
---
+++ EffectiveEx/DataGridForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EffectiveEx
+{
+    public partial class DataGridForm : Form
+    {
+
+        private Form1 main_form;
+
+        //コンストラクタ
+        public DataGridForm()
+        {
+            InitializeComponent();
+            gridTableView.RowHeadersDefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            gridTableView.AllowUserToAddRows = false;
+        }
+
+        //データグリッドセットアップ
+        public void init(DataTable tbl, string title)
+        {
+            this.Text = title;
+            gridTableView.DataSource = tbl;
+        }
+    }
+}
