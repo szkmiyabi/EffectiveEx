8a294dfd60766c459f1a85ea680386f3b5753996
szkmiyabi <led.centipede@gmail.com>
2020-12-13 20:03:44 +0900

    ソリューションファイルのプロパティを設定
    EffectiveExのプロパティ→アプリケーション→アセンブリ情報→会社名に団体名を記述

--- EffectiveEx/Properties/AssemblyInfo.cs
+++ EffectiveEx/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("EffectiveEx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("JCI-TN")]
 [assembly: AssemblyProduct("EffectiveEx")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
